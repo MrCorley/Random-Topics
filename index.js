d34904f4469fa182533ef5b0f6f5468448d031fa
Jonathan Corley <>
2021-04-30 19:45:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+//Assignment Operator
+//=
+
+//Increment 
+//++
+
+//Decrement 
+//--
+
+//Math Operators
+// + 
+// - 
+// *
+// /
+// %
+// **
+
+//Compound Operators
+// +=
+// -=
+// *=
+// /=
+// **=
+
+
+/////////////////////
+// BEST PRACTICIES //
+/////////////////////
+
+//What are comments for?
+
+
+
+//Why do we use indentations?
+
+
+
+
+//What naming conventions are used?
+
+
+
+//what are constants?
+
+
+
+//What are reserved keywords?
+
+
+
+//debugger keyword
+//allows programmer to fix errors in code by running it "line by line"
+
+
+//setting breakpoints 
+//allows programmer to see what is happening at specific points in code
+
+
+//What is console.log used for?
+
+
+
+
+//Can you describe all of these primitive datatypes
+//number
+//boolean
+//string
+//null 
+//undefined
+
+ //what datatypes does the typeof operator not work for?
+ //arrays and null
+
+//string operations
+//you can concat or join strings with the join operator(+)
+//you cannot change them but they are indexed like arrays
+//there is a method that can split strings into a single array called .split()
+var str = 'hello my name is joe'
+var splitStr = str.split('')
+console.log(splitStr)
+
+
+//single quote vs double quote (nesting)
+'this has double quotes inside of it "look here" '
+"this has single quotes inside of it 'look here' "
+
+
+//declaration
+var num1;
+
+//assigning
+num1 = 9
+
+//initialization
+var num2 = 10
